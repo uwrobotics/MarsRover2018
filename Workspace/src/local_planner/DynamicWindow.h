9ee80669147c1d6eb3a0496b5e2ea685934b7580
Tom Meredith <tommeredith14@gmail.com>
2018-02-22 16:32:59 -0500

    start work on local_planner

--- Workspace/src/local_planner/DynamicWindow.h
+++ Workspace/src/local_planner/DynamicWindow.h
@@ -2,12 +2,36 @@
 #define DYNAMICWINDOW_H
 #include "ros/ros.h"
 
+
+
+//This class implements the logic for a single cycle's
+//Dynamic window, including creating the window,
+//assessing the points for validity, and scoring them
 class CDynamicWindow
 {
 public:
     CDynamicWindow();
 
 private:
+    class DynamicWindowPoint
+    {
+        DynamicWindowPoint(float vel, float angvel)
+        : v(vel),
+          w(angvel),
+          dist(0),
+          score(0)
+        {
+        }
+        float v;
+        float w;
+        float dist;
+        float score;
+    };
+
+    //The dynamic window grid
+    typedef std::vector< DynamicWindowPoint > VelocityRow;
+    std::vector < VelocityRow > m_dynamicWindowGrid;
+
 
 };
 
