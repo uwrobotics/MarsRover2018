bad08d3e52040f2c7cea0c4d027b621fa0eaf21a
Tom Meredith <tommeredith14@gmail.com>
2018-02-27 21:38:54 -0500

    added heading calculation

--- Workspace/src/local_planner/src/LocalPlanner.cpp
+++ Workspace/src/local_planner/src/LocalPlanner.cpp
@@ -3,6 +3,26 @@
 //
 
 #include "LocalPlanner.h"
+#include "robot_localization/navsat_conversions.h"
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/buffer.h>
+#include <tf/tf.h>
+
+
+#include <robot_localization/navsat_conversions.h>
+//#include <move_base_msgs/MoveBaseAction.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <actionlib/client/simple_action_client.h>
+#include "std_msgs/String.h"
+#include <string>
+#include <cmath>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/buffer.h>
+//#include <rover_autonomy/gps_coord.h>
+#include <visualization_msgs/Marker.h>
+#include <std_msgs/Int32MultiArray.h>
 
 CLocalPlanner::CLocalPlanner(ros::NodeHandle *pNh, const RobotParams_t& robotParams)
  : m_pNh(pNh),
@@ -21,6 +41,9 @@ CLocalPlanner::CLocalPlanner(ros::NodeHandle *pNh, const RobotParams_t& robotPar
             m_pNh->subscribe("cur_gps",1,&CLocalPlanner::CurGPSCallback, this));
     m_pGoalGpsSub = new ros::Subscriber(
             m_pNh->subscribe("goal_gps",1,&CLocalPlanner::GoalGPSCallback, this));
+    m_pOdometrySub = new ros::Subscriber(
+            m_pNh->subscribe("odometry",1,&CLocalPlanner::OdometryCallback, this));
+
     m_pVelPub = new ros::Publisher(m_pNh->advertise<geometry_msgs::Twist>("local_planner_vel",1));
 }
 
@@ -32,12 +55,54 @@ void CLocalPlanner::CurVelCallback(geometry_msgs::Twist::ConstPtr vel)
 void CLocalPlanner::GoalGPSCallback(sensor_msgs::NavSatFix::ConstPtr goal)
 {
     m_goalGPS = *goal;
+    RobotLocalization::NavsatConversions::LLtoUTM(goal->latitude,
+                                                  goal->longitude,
+                                                  m_goalGpsUtmX,
+                                                  m_goalGpsUtmY,
+                                                  m_goalGpsUtmZone);
+
 }
 
 void CLocalPlanner::CurGPSCallback(sensor_msgs::NavSatFix::ConstPtr gps)
 {
     m_curGPS = *gps;
 }
+
+void CLocalPlanner::OdometryCallback(nav_msgs::Odometry::ConstPtr odometry)
+{
+
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+    geometry_msgs::TransformStamped roverLocToUTM;
+    try{
+        roverLocToUTM = tfBuffer.lookupTransform("utm", odometry->child_frame_id, ros::Time(0), ros::Duration(2));
+    }
+    catch (tf2::TransformException& ex ){
+        ROS_ERROR("%s",ex.what());
+    }
+
+    ROS_INFO("Generated transform from base_link to utm");
+    m_curGpsUtmX = roverLocToUTM.transform.translation.x;
+    m_curGpsUtmY = roverLocToUTM.transform.translation.y;
+
+
+    m_curVel = odometry->twist.twist;
+
+    double heading = 0;////TODO
+    tf::Quaternion q(
+            odometry->pose.pose.orientation.x,
+            odometry->pose.pose.orientation.y,
+            odometry->pose.pose.orientation.z,
+            odometry->pose.pose.orientation.w);
+    tf::Matrix3x3 m(q);
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+    heading = yaw;
+
+    m_orientationToGoal = atan2(-(m_goalGpsUtmX - m_curGpsUtmX),
+                                m_goalGpsUtmY - m_curGpsUtmY) - heading;
+}
+
 
 /*
 ///Occupancy Callback:
@@ -55,7 +120,7 @@ void CLocalPlanner::OccupancyCallback(occupancy_grid::OccupancyGrid::ConstPtr gr
 
     CDynamicWindow dynamicWindow(m_curVel.linear.x,m_curVel.angular.z,m_robotParams);
 
-    geometry_msgs::Twist chosenVel = dynamicWindow.AssessOccupancyGrid(grid,0);
+    geometry_msgs::Twist chosenVel = dynamicWindow.AssessOccupancyGrid(grid,m_orientationToGoal);
 
     m_pVelPub->publish(chosenVel);
 }
