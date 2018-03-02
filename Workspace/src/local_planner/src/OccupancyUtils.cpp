1d33d4696bc5e024543f3fe1fcfae4b3b9b2dc47
Aditya Matam <mataditya@gmail.com>
2018-03-01 21:44:25 -0500

    debugging

--- Workspace/src/local_planner/src/OccupancyUtils.cpp
+++ Workspace/src/local_planner/src/OccupancyUtils.cpp
@@ -2,28 +2,36 @@
 // Created by tom on 24/02/18.
 //
 #include "OccupancyUtils.h"
+#include <ros/ros.h>
+#define HEIGHT_THRESH 0.15
 
 namespace OccupancyUtils {
 
 
 //accessor for Occupancy Grid Message
-    float oGridDataAccessor (occupancy_grid::OccupancyGrid::ConstPtr oGrid, unsigned int i, unsigned int j, unsigned int  k) {
+    float oGridDataAccessor (occupancy_grid::OccupancyGrid::ConstPtr& oGrid, unsigned int i, unsigned int j, unsigned int  k) {
         return oGrid->data[i*oGrid->dataDimension[0].stride + j*oGrid->dataDimension[1].stride + k*oGrid->dataDimension[2].stride];
     }
 
     void PointForCoord(occupancy_grid::OccupancyGrid::ConstPtr& pGrid, double y, double x, int& zOut, int& xOut)
     {
         float coordZ = (y)/pGrid->header.gridResolution;
-        float coordX = (-x)/pGrid->header.gridResolution + (pGrid->dataDimension[0].stride/2.0);
+        float coordX = (-x)/pGrid->header.gridResolution + (pGrid->dataDimension[1].size/2.0);
         zOut = (int)(coordZ + 0.5);
         xOut = (int)(coordX + 0.5);
     }
+
+void odbg(double x, double y, int xi, int yi, occupancy_grid::OccupancyGrid::ConstPtr& grid)
+{
+ROS_INFO("checking (%f,%f) --> (%d,%d) -- height=%f",x,y,xi,yi,oGridDataAccessor(grid,yi,xi,1));
+}  
 
     double CalcDistance(occupancy_grid::OccupancyGrid::ConstPtr& pGrid,
                         float v, float w,
                         float robotLength, float robotWidth,
                         float timestep)
     {
+ROS_INFO("v=%f,w=%f",v,w);
         double radius = ((double) v) / w;
         double retDist = 0;
         ///////////
@@ -32,7 +40,7 @@ namespace OccupancyUtils {
 //                y = 0;%m
 //                distanceMax=10;
 //
-        double safetyBubble = 0.3 * v;
+        double safetyBubble = 0.0 * v;
         double bufferFromCenter = safetyBubble + robotWidth / 2;
 
         //not moving
@@ -41,9 +49,9 @@ namespace OccupancyUtils {
             int xIndexCenter = 0;//size(OccupancyGrid,2)/2;
             int xIndexLeftCorn = 0;
             int xIndexRightCorn = 0;
-            PointForCoord(pGrid, robotLength / 2, 0, yIndex, xIndexCenter);
-            PointForCoord(pGrid, robotLength / 2, bufferFromCenter, yIndex, xIndexLeftCorn);
-            PointForCoord(pGrid, robotLength / 2, -bufferFromCenter, yIndex, xIndexRightCorn);
+            PointForCoord(pGrid, 0/*robotLength / 2*/, 0, yIndex, xIndexCenter);
+            PointForCoord(pGrid, 0/*robotLength / 2*/, bufferFromCenter, yIndex, xIndexLeftCorn);
+            PointForCoord(pGrid, 0/*robotLength / 2*/, -bufferFromCenter, yIndex, xIndexRightCorn);
 
 //        xIndexRightCorn = ceil(xIndexCenter + bufferFromCenter/resolution);
 //        xIndexLeftCorn = floor(xIndexCenter - bufferFromCenter/resolution);
@@ -52,9 +60,12 @@ namespace OccupancyUtils {
 //        else
 //            distance = distanceMax;
 //        end
-            if (oGridDataAccessor(pGrid,yIndex, xIndexCenter, 0) != 0 ||
-                    oGridDataAccessor(pGrid,yIndex, xIndexRightCorn, 0) != 0 ||
-                    oGridDataAccessor(pGrid,yIndex, xIndexLeftCorn, 0) != 0)
+odbg(0,0,xIndexCenter,yIndex,pGrid);
+odbg(-bufferFromCenter,0,xIndexLeftCorn,0,pGrid);
+odbg(bufferFromCenter,0,xIndexRightCorn,0,pGrid);
+            if (oGridDataAccessor(pGrid,yIndex, xIndexCenter, 1) > HEIGHT_THRESH ||
+                    oGridDataAccessor(pGrid,yIndex, xIndexRightCorn, 1) > HEIGHT_THRESH ||
+                    oGridDataAccessor(pGrid,yIndex, xIndexLeftCorn, 1) > HEIGHT_THRESH)
             {
                 retDist = 0;
             }
@@ -71,9 +82,9 @@ namespace OccupancyUtils {
             int xIndexCenter = 0;//size(OccupancyGrid,2)/2;
             int xIndexLeftCorn = 0;
             int xIndexRightCorn = 0;
-            PointForCoord(pGrid, robotLength / 2, 0, yIndex, xIndexCenter);
-            PointForCoord(pGrid, robotLength / 2, bufferFromCenter, yIndex, xIndexLeftCorn);
-            PointForCoord(pGrid, robotLength / 2, -bufferFromCenter, yIndex, xIndexRightCorn);
+            PointForCoord(pGrid, 0/*robotLength / 2*/, 0, yIndex, xIndexCenter);
+            PointForCoord(pGrid, 0/*robotLength / 2*/, bufferFromCenter, yIndex, xIndexLeftCorn);
+            PointForCoord(pGrid, 0/*robotLength / 2*/, -bufferFromCenter, yIndex, xIndexRightCorn);
             double distance = -1.0;
             bool continueChecking = true;
 
@@ -86,9 +97,12 @@ namespace OccupancyUtils {
 //
             while (continueChecking)
             {
-                if (oGridDataAccessor(pGrid,yIndex, xIndexCenter, 0) != 0 ||
-                    oGridDataAccessor(pGrid,yIndex, xIndexRightCorn, 0) != 0 ||
-                    oGridDataAccessor(pGrid,yIndex, xIndexLeftCorn, 0) != 0)
+odbg(0,yIndex*pGrid->header.gridResolution, xIndexCenter,yIndex,pGrid);
+odbg(-bufferFromCenter,yIndex*pGrid->header.gridResolution,xIndexLeftCorn,yIndex,pGrid);
+odbg(bufferFromCenter,yIndex*pGrid->header.gridResolution,xIndexRightCorn,yIndex,pGrid);
+                if (oGridDataAccessor(pGrid,yIndex, xIndexCenter, 1) > HEIGHT_THRESH ||
+                    oGridDataAccessor(pGrid,yIndex, xIndexRightCorn, 1) > HEIGHT_THRESH ||
+                    oGridDataAccessor(pGrid,yIndex, xIndexLeftCorn, 1) > HEIGHT_THRESH)
                 {
                     continueChecking = false;
                     retDist = yIndex * pGrid->header.gridResolution;// - timestep * v;
@@ -98,7 +112,7 @@ namespace OccupancyUtils {
                     yIndex = yIndex + 1;
                     if (yIndex > pGrid->dataDimension[0].size)
                     {
-                        distance = DISTANCE_MAX;// -velocity(1) * timestep;
+                        retDist = DISTANCE_MAX;// -velocity(1) * timestep;
                         continueChecking=false;
 //        end
 //                end
@@ -116,7 +130,7 @@ namespace OccupancyUtils {
             double dTheta = ds/radius;
 //
             bool done = false;
-            double theta = 0 + robotLength/2/radius;//Start checking from the front of the robot
+            double theta = 0;// + robotLength/2/radius;//Start checking from the front of the robot
 //
             double centreX = -radius;
             double centreY = 0;
@@ -163,16 +177,22 @@ namespace OccupancyUtils {
                     retDist = distTravelled - timestep * v;
                     done = true;
                 }
-                else if ((oGridDataAccessor(pGrid,centerYIndex,centerXIndex,0) != 0) ||
-                        (oGridDataAccessor(pGrid,rightYIndex,rightXIndex,0) != 0) ||
-                        (oGridDataAccessor(pGrid,leftYIndex,leftXIndex,0) != 0))
+                else
+{
+odbg(frontCenterX,frontCenterY, centerXIndex,centerYIndex,pGrid);
+odbg(frontRightX,frontRightY, rightXIndex,rightYIndex,pGrid);
+odbg(frontLeftX,frontLeftY, leftXIndex,leftYIndex,pGrid);
+ if ((oGridDataAccessor(pGrid,centerYIndex,centerXIndex,0) > HEIGHT_THRESH) ||
+                        (oGridDataAccessor(pGrid,rightYIndex,rightXIndex,0) > HEIGHT_THRESH) ||
+                        (oGridDataAccessor(pGrid,leftYIndex,leftXIndex,0) > HEIGHT_THRESH))
                 {
                     retDist = distTravelled - timestep * v;
                     done = true;
 //        end
                 }
 //                end
-            }
+} 
+           }
 //
 
         }
