a8364877d32ad9c65e670c027722bb66e2b34062
Archie Lee <archielee@users.noreply.github.com>
2018-05-07 23:57:02 -0400

    Revert previous commit

--- ci/check_formatting.bash
+++ ci/check_formatting.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 find $CI_SOURCE_PATH/Workspace/src -name '*.cpp' -name '*.h' -name '*.hpp' | xargs -n1 clang-format -style=file -output-replacements-xml | grep "<replacement " > /dev/null
-if [ $? -ne 0 ]
+if [ $? -ne 1 ]  # this should fail (should not find anything)
 then
     echo "Incorrect formatting in some C++ files, run clang-format to fix"
     exit 1;
@@ -8,7 +8,7 @@ fi
 
 sudo pip install -r $CI_SOURCE_PATH/ci/requirements.txt
 find $CI_SOURCE_PATH/Workspace/src -name '*.py' -exec pycodestyle {} \; | grep ".*" > /dev/null
-if [ $? -ne 0 ]
+if [ $? -ne 1 ]  # this should fail (should not find anything)
 then
     echo "Incorrect formatting in some Python files, check formatting using pycodestyle"
     exit 1;
