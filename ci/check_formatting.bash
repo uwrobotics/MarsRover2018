5f12f37d6b2d0e9cce341dfec48ee10591b6fa8d
Archie Lee <archielee@users.noreply.github.com>
2018-05-11 23:28:56 -0400

    ci: Update check_formatting script to not check dependencies

--- ci/check_formatting.bash
+++ ci/check_formatting.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 find $CI_SOURCE_PATH/Workspace/src -name '*.cpp' -name '*.h' -name '*.hpp' | xargs -n1 clang-format -style=file -output-replacements-xml | grep "<replacement " > /dev/null
 if [ $? -ne 1 ]  # this should fail (should not find anything)
 then
@@ -7,7 +8,7 @@ then
 fi
 
 sudo pip install -r $CI_SOURCE_PATH/ci/requirements.txt
-find $CI_SOURCE_PATH/Workspace/src -name '*.py' -exec pycodestyle {} \; | grep ".*" > /dev/null
+find $CI_SOURCE_PATH/Workspace/src -path $CI_SOURCE_PATH/Workspace/src/dependencies -prune -o -name '*.py' -exec pycodestyle {} \; | grep ".*" > /dev/null
 if [ $? -ne 1 ]  # this should fail (should not find anything)
 then
     echo "Incorrect formatting in some Python files, check formatting using pycodestyle"
