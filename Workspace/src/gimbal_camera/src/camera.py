a56493d47cfecab9aff6d119d3fd841e7a57dbab
Zameer <you@example.com>
2018-05-28 03:09:22 -0400

    updated with corrections for CAN ID

--- Workspace/src/gimbal_camera/src/camera.py
+++ Workspace/src/gimbal_camera/src/camera.py
@@ -15,7 +15,7 @@ class GimbalController:
                                           queue_size=20)
         self.pan = 0
         self.tilt = 0
-        self.ID = 600
+        self.ID = 510
 
     def callback(self, joy):
 
