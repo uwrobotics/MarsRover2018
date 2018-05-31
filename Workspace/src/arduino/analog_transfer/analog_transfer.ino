c6d02c8e618b27154aed25a000a3f6a4d56d5a6c
Jerry Li <jerryli033@gmail.com>
2018-05-31 00:58:21 -0700

    update arduino code for arduino uno

--- Workspace/src/arduino/analog_transfer/analog_transfer.ino
+++ Workspace/src/arduino/analog_transfer/analog_transfer.ino
@@ -1,4 +1,4 @@
-const int RPI_DATA_REQUEST_PIN   = 22; // Active high
+const int RPI_DATA_REQUEST_PIN   = 7; // Active high
 const int ENCODER_PIN            = 3;  // Analog
 
 bool toWrite = true;
@@ -6,7 +6,6 @@ bool toWrite = true;
 void setup() {
   pinMode(RPI_DATA_REQUEST_PIN, INPUT);
   //pinMode (ENCODER_PIN, INPUT);
-  Serial3.begin(9600);  
   Serial.begin(9600);
 }
 
@@ -14,7 +13,6 @@ void send_analog_data()
 {
    int pwm_in = analogRead(ENCODER_PIN);
    Serial.println (pwm_in);
-   Serial3.println(pwm_in);
 }
 
 void loop() {  
